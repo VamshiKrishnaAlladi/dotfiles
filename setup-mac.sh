22e6d033f3fb62e38d19372ba0386b971a9c4f70
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2020-02-09 16:51:21 +0530

    feat(setup): implement platform specific setup scripts

---
+++ setup-mac.sh
@@ -0,0 +1,6 @@
+link .bash_profile ~/.bash_profile
+link .bashrc ~/.bashrc
+link .czrc ~/.czrc
+link .gitconfig ~/.gitconfig
+link git-completion.bash ~/git-completion.bash
+link mac.sh ~/mac.sh
---
+++ setup-windows.sh
@@ -0,0 +1,6 @@
+link .bash_profile ~/.bash_profile
+link .bashrc ~/.bashrc
+link .czrc ~/.czrc
+link .gitconfig ~/.gitconfig
+link git-completion.bash ~/git-completion.bash
+link windows.sh ~/windows.sh
